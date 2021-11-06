6a0bbd79abdf4073e72643ac0cec63c3352a0767
Maciej Jordanek <maciej.jordanek@ready2order.com>
2021-11-06 20:23:39 +0100

    ok, now its next

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,96 @@
-import Head from 'next/head'
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const [currentProduct, setCurrentProduct] = useState("");
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoading) {
+      setTimeout(() => {
+        router.push({
+          pathname: "/result",
+          query: { result: "foo" }
+        });
+      }, 3000);
+    }
+  }, [isLoading]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-full w-full flex flex-col justify-center items-center">
+      <div className="flex space-x-2">
+        <input
+          value={currentProduct}
+          onChange={(e) => {
+            setCurrentProduct(e.target.value);
+          }}
+          className="border-solid border-2 p-2"
+        />
+        <button
+          onClick={() => {
+            setProducts([...products, currentProduct]);
+            setCurrentProduct("");
+          }}
+          className="border-solid border-2 p-2"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+          Add product
+        </button>
+      </div>
+
+      <div>
+        <input
+          type="file"
+          accept="image/*"
+          onLoad={console.log}
+          onChange={(e) => {
+            const file = e.target.files?.[0];
+
+            console.log(e);
+            // async function quickstart() {
+            //   // Imports the Google Cloud client library
+            //   const vision = require("@google-cloud/vision");
+
+            //   // Creates a client
+            //   const client = new vision.ImageAnnotatorClient();
+
+            //   // Performs label detection on the image file
+            //   const [result] = await client.labelDetection(e.target.value);
+            //   const labels = result.labelAnnotations;
+            //   console.log("Labels:", labels);
+            // }
+            // quickstart();
+            if (file) {
+              const reader = new FileReader();
+              reader.onload = function () {
+                console.log(reader.result);
+                setIsLoading(true);
+              };
+
+              reader.readAsBinaryString(file);
+            }
+          }}
+        />
+
+        {isLoading && <div>loading data...</div>}
+      </div>
+
+      {products.map((product, index) => {
+        return (
+          <div className="flex space-x-2">
+            <div>{product}</div>
+            <button
+              className=""
+              onClick={() => {
+                setProducts(products.filter((_, i) => i !== index));
+              }}
+            >
+              -
+            </button>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
---
+++ pages/result.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Result() {
+  return <div>you still need to buy:</div>;
+}

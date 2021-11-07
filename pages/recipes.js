8754b141e59403856b7e9f25ef2dad29e25747f5
Maciej Jordanek <maciej.jordanek@ready2order.com>
2021-11-07 13:44:57 +0100

    style second page

--- pages/recipes.js
+++ pages/recipes.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Loader from "../components/Loader";
+import Image from "next/image";
 
 const getMissingProducts = async (ingredients) => {
   const res = await fetch(`/api/missingProducts?ingredients=${ingredients}`);
@@ -25,27 +26,47 @@ export default function Home() {
   }
 
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center">
-      <div className="grid grid-cols-3">
+    <div className="h-full w-full flex flex-col justify-center items-center bg-light-green p-4">
+      <button
+        className="w-full flex justify-start mb-4"
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        <Image
+          src="/assets/arrow-right.svg"
+          height="30"
+          width="30"
+          className="rotate-180"
+        />
+      </button>
+      <div className="flex flex-col space-y-4 overflow-scroll">
         {recipes.map((recipe) => {
           return (
-            <button
-              onClick={() => {
-                router.push({
-                  pathname: "/result",
-                  query: {
-                    recipeId: recipe.id,
-                    missedIngredients: recipe.missedIngredients
-                      .map((ingredient) => ingredient.name.toLowerCase())
-                      .join(",")
-                  }
-                });
-              }}
+            <div
               key={recipe.id}
+              className="flex bg-white rounded-2xl justify-center items-center"
             >
-              {recipe.title}
-              <img src={recipe.image} className="w-24 h-24 rounded-xl" />
-            </button>
+              <img src={recipe.image} className="w-32 h-32 rounded-xl" />
+              <div className="text-xl text-left mx-2">{recipe.title}</div>
+
+              <button
+                onClick={() => {
+                  router.push({
+                    pathname: "/result",
+                    query: {
+                      recipeId: recipe.id,
+                      missedIngredients: recipe.missedIngredients
+                        .map((ingredient) => ingredient.name.toLowerCase())
+                        .join(",")
+                    }
+                  });
+                }}
+                className="text-light-green"
+              >
+                <div className="mr-4 underline">choose this recipe</div>
+              </button>
+            </div>
           );
         })}
       </div>
